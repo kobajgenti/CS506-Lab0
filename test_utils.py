6bb9c6e5433e63602709d6ade6aba8eb4a6a6102
Ross Mikulskis <rkulskis@bu.edu>
2024-09-13 10:56:40 -0400

    fix: removed git merge conflict info

--- test_utils.py
+++ test_utils.py
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-## Please fill in all the parts labeled as ### YOUR CODE HERE
-
-import numpy as np
-import pytest
-from utils import *
-
-def test_dot_product():
-    vector1 = np.array([1, 2, 3])
-    vector2 = np.array([4, 5, 6])
-    
-    result = dot_product(vector1, vector2)
-    
-    assert result == 32, f"Expected 32, but got {result}"
-    
-def test_cosine_similarity():
-    ### YOUR CODE HERE
-    
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
-    
-    assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
-
-def test_nearest_neighbor():
-    ### YOUR CODE HERE
-    
-    result = ### YOUR CODE HERE
-    
-    expected_index = ### YOUR CODE HERE
-    
-    assert result == expected_index, f"Expected index {expected_index}, but got {result}"
-=======
 ## Please fill in all the parts labeled as ### YOUR CODE HERE
 
 import numpy as np
@@ -68,9 +35,3 @@ def test_nearest_neighbor():
     expected_index = 1 ### YOUR CODE HERE
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
-
-test_dot_product()
-test_nearest_neighbor()
-test_cosine_similarity()
-
->>>>>>> rkulskis
